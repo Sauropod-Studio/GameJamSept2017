d772f5c3a977da42914549def3c408d0884ffa12
benoit-sauropod <benoit@sauropodstudio.com>
2017-09-23 19:46:03 -0400

    fixes de montgolfiere de francois

--- Assets/Francois/PiloteDeMongolefiere.cs
+++ Assets/Francois/PiloteDeMongolefiere.cs
@@ -3,37 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PiloteDeMongolefiere : MonoBehaviour {
+    public float acceleration;
+    public GameObject PivotDeCamera;
+    private RegarderAvecLesPieds _pieds;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    private void Start()
+    {
+        _pieds = GetComponent<RegarderAvecLesPieds>();
+    }
+    // Update is called once per frame
+    void Update () {
         if (Input.GetKey(KeyCode.W))
         {
-            GetComponent<Rigidbody>().velocity += transform.forward * 0.5f;
+            GetComponent<Rigidbody>().velocity += transform.forward * acceleration;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            GetComponent<Rigidbody>().velocity += transform.forward * -0.5f;
+            GetComponent<Rigidbody>().velocity += transform.forward * -acceleration;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            GetComponent<Rigidbody>().velocity += transform.right * -0.5f;
+            GetComponent<Rigidbody>().velocity += transform.right * -acceleration;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            GetComponent<Rigidbody>().velocity += transform.right * 0.5f;
+            GetComponent<Rigidbody>().velocity += transform.right * acceleration;
         }
 
         if(Input.GetKey(KeyCode.Space))
         {
-            GetComponent<Rigidbody>().velocity += transform.up * -0.5f;
+            GetComponent<Rigidbody>().velocity += transform.up * -acceleration;
+        }
+
+        RaycastHit hit;
+        var gravite = (_pieds.Referent - transform.position).normalized;
+        if (Physics.Raycast(transform.position, gravite, out hit, _pieds.hauteurVoulue))
+        {
+            PivotDeCamera.transform.position = transform.position + gravite * hit.distance;
         }
     }
 }
--- Assets/Francois/RegarderAvecLesPieds.cs
+++ Assets/Francois/RegarderAvecLesPieds.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class RegarderAvecLesPieds : MonoBehaviour {
     public Vector3 Referent;
     public float hauteurVoulue;
+    public LayerMask masque = 1 << 8;
 	// Update is called once per frame
 	void Update () {
         var orientationLocale = transform.InverseTransformPoint(Referent);
@@ -14,7 +15,7 @@ public class RegarderAvecLesPieds : MonoBehaviour {
         //rigidbody.velocity += -transform.position.normalized * (Vector3.Distance(transform.position, Vector3.zero) - hauteurVoulue) * 0.1f;
 
         var gravite = (Referent - transform.position).normalized * 0.1f;
-        if (Physics.Raycast(transform.position, gravite, hauteurVoulue))
+        if (Physics.Raycast(transform.position, gravite, hauteurVoulue,masque))
         {
             rigidbody.velocity -= gravite;
         }
