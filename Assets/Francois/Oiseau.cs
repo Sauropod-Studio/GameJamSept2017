28975d91615fb1f10f490808d46297f432bb5f3d
benoit-sauropod <benoit@sauropodstudio.com>
2017-09-23 19:45:46 -0400

    Ajout du package d'oiseau

---
+++ Assets/Francois/Oiseau.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oiseau : MonoBehaviour {
+    public float acceleration;
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody>().velocity += transform.forward * acceleration;
+        if(Random.value < 0.01)
+        {
+            GetComponent<Rigidbody>().angularVelocity += transform.up * (Random.value - 0.5f) * 2f;
+        }
+    }
+}
