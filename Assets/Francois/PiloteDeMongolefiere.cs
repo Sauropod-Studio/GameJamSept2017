0a1d3b82aebfc70108fc463def1316ba3682930a
benoit-sauropod <benoit@sauropodstudio.com>
2017-09-23 14:00:59 -0400

    Added Francois's stuff from unitypackage.

---
+++ Assets/Francois/PiloteDeMongolefiere.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiloteDeMongolefiere : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Rigidbody>().velocity += transform.forward * 0.5f;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Rigidbody>().velocity += transform.forward * -0.5f;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody>().velocity += transform.right * -0.5f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody>().velocity += transform.right * 0.5f;
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().velocity += transform.up * -0.5f;
+        }
+    }
+}
---
+++ Assets/Francois/RegarderAvecLesPieds.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegarderAvecLesPieds : MonoBehaviour {
+    public Vector3 Referent;
+    public float hauteurVoulue;
+	// Update is called once per frame
+	void Update () {
+        var orientationLocale = transform.InverseTransformPoint(Referent);
+        transform.Rotate(-Clamp(orientationLocale.z), 0, Clamp(orientationLocale.x));
+
+        var rigidbody = GetComponent<Rigidbody>();
+        //rigidbody.velocity += -transform.position.normalized * (Vector3.Distance(transform.position, Vector3.zero) - hauteurVoulue) * 0.1f;
+
+        var gravite = (Referent - transform.position).normalized * 0.1f;
+        if (Physics.Raycast(transform.position, gravite, hauteurVoulue))
+        {
+            rigidbody.velocity -= gravite;
+        }
+        else
+        {
+            rigidbody.velocity += gravite*2f;
+        }
+    }
+    private float Clamp(float value, float bounds = 1f)
+    {
+        return Mathf.Clamp(value, -bounds, bounds);
+    }
+}
